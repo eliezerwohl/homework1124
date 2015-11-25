78b367b4b3c65212f73be24b5bb46b8baa85daa0
eliezerwohl <eliezerwohl@gmail.com>
2015-11-25 14:26:33 -0500

    added basic containers, rows, columns

--- js/js.js
+++ js/js.js
@@ -1,1 +1,36 @@
-prompt("herro");
+$("Document").ready(function() {
+
+  $(".btn-default").on("click", function() {
+
+
+var state1, searchButton, nj, pa
+nj = "nj", "NJ";
+ny = "ny";
+pa = "pa";
+state1 = $("#state1").val();
+
+
+if (state1 === nj) {
+    $(".nj").fadeIn(2000);
+    $(".pa").fadeOut(2000);
+    $(".ny").fadeOut(2000);
+     return;
+}
+
+if (state1 === ny) {
+  $(".ny").fadeIn(2000);
+
+  return;
+
+
+}
+
+else {
+  prompt("please enter something");
+}
+
+
+});
+
+
+});
