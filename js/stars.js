fc362f757bb2626f2a1ed4e0d1a348e19e31d1b3
eliezerwohl <eliezerwohl@gmail.com>
2015-11-26 12:50:33 -0500

    fixed sorting by stars, cleaned up html

--- js/stars.js
+++ js/stars.js
@@ -1,31 +1,31 @@
 
 $("Document").ready(function() {
 
-// $("#1star").on("click", function() {
-//     $(".5star").show();
-//     $(".4star").show();
-//     $(".3star").show();
-//     $(".2star").show();
-//     $(".1star").show();
+$(".1input").on("click", function() {
+    $(".5star").show();
+    $(".4star").show();
+    $(".3star").show();
+    $(".2star").show();
+    $(".1star").show();
     
-//   });
-//   $("#2star").on("click", function() {
-//     $(".5star").show();
-//     $(".4star").show();
-//     $(".3star").show();
-//     $(".2star").show();
-//     $(".1star").hide();
+  });
+  $(".2input").on("click", function() {
+    $(".5star").show();
+    $(".4star").show();
+    $(".3star").show();
+    $(".2star").show();
+    $(".1star").hide();
     
-//   });
-//   $(".star3").on("click", function() {
-//     $(".5star").show();
-//     $(".4star").show();
-//     $(".3star").show();
-//     $(".2star").hide();
-//     $(".1star").hide();
+  });
+  $(".3input").on("click", function() {
+    $(".5star").show();
+    $(".4star").show();
+    $(".3star").show();
+    $(".2star").hide();
+    $(".1star").hide();
    
 
-//   });
+  });
   $(".4input").on("click", function() {
     $(".5star").show();
     $(".4star").show();
@@ -36,7 +36,6 @@ $("Document").ready(function() {
   });
   $(".5input").on("click", function() {
     $(".5star").show();
-    
     $(".4star").hide();
     $(".3star").hide();
     $(".2star").hide();
