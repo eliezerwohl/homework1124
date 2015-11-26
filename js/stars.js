3cd3bce9171c76e8a1817ac08c5f8336a1fbb5d4
eliezerwohl <eliezerwohl@gmail.com>
2015-11-26 08:37:00 -0500

    added radio buttons, 1-5 stars, displays only that amount of stars, doesn't display other states

---
+++ js/stars.js
@@ -0,0 +1,50 @@
+// $("Document").ready(function() {
+ 
+
+
+// // $("#1star").on("click", function() {
+// //     $(".5star").show();
+// //     $(".4star").show();
+// //     $(".3star").show();
+// //     $(".2star").show();
+// //     $(".1star").show();
+    
+//   // });
+//   // $("#2star").on("click", function() {
+//   //   $(".5star").show();
+//   //   $(".4star").show();
+//   //   $(".3star").show();
+//   //   $(".2star").show();
+//   //   $(".1star").hide();
+    
+//   // });
+//   $(".star3").on("click", function() {
+//     $(".5star").show();
+//     $(".4star").show();
+//     $(".3star").show();
+//     $(".2star").hide();
+//     $(".1star").hide();
+   
+
+//   });
+//   $(".star4").on("click", function() {
+//     $(".5star").show();
+//     $(".4star").show();
+//     $(".3star").hide();
+//     $(".2star").hide();
+//     $(".1star").hide();
+    
+//   });
+//   // $("#5star").on("click", function() {
+//   //   $(".5star").show();
+//   //   $(".4star").hide();
+//   //   $(".3star").hide();
+//   //   $(".2star").hide();
+//   //   $(".1star").hide();
+
+
+//   // });
+//   });
+
+
+
