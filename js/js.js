858e4836d70a212b60e8752afd7d93ec4b36d192
eliezerwohl <eliezerwohl@gmail.com>
2015-11-27 15:50:00 -0500

    all fields must be filled in order to search

--- js/js.js
+++ js/js.js
@@ -2,11 +2,9 @@ $("Document").ready(function() {
 
   $(".resetSearch").hide();
 
-    $(".btn-default").on("click", function() {
-    $('.btn-default').hide();
-    $(".resetSearch").show();
-    
-    var stateSearch, searchButton, nj, acceptNJ, acceptNY, acceptPA, pa, domtest,
+    $(".btn-default").on("click", function() {   
+ 
+    var stateSearch, searchButton, nj, acceptNJ, acceptNY, acceptPA, pa, domtest, endEmpty
     // dictionary
     acceptNJ = {
         "nj" : true,
@@ -28,7 +26,30 @@ $("Document").ready(function() {
     ny = "ny";
     pa = "pa";
     stateSearch = $("#stateSearch").val();
+    var endEmpty, startEmpty, stateEmpty
+   stateSearch = $("#stateSearch").val();
+      endEmpty= $("#endDate").val();
+      startEmpty= $("#startDate").val();
 
+       if (endEmpty.length === 0) {
+      prompt("needs ending");
+      return;
+     
+    };
+
+       if (startEmpty.length === 0) {
+      prompt("needs start");
+      return;
+    };
+
+
+       if (stateSearch.length === 0) {
+      prompt("enter in a state");
+    return;
+    };
+
+  
+    
     if (stateSearch === domtest) {
 
     $(".target").prepend("<div>").addClass("panel panel-warning nj 5star").prepend("<div class='panel panel-body'>nj one star </div></div></div>");
@@ -44,7 +65,8 @@ $("Document").ready(function() {
     $(".target").prepend("<div class='panel panel-warning njpanel 3star wifi'> <div class='panel panel-body'>3 star nj wifi</div></div>");
     $(".target").prepend("<div class='panel panel-warning njpanel 2star hbo'> <div class='panel panel-body'>2 star nj hbo</div></div>");
     $(".target").prepend("<div class='panel panel-warning njpanel 1star nothing'> <div class='panel panel-body'>1 star nj</div></div>");
-
+ $('.btn-default').hide();
+    $(".resetSearch").show();
          return;
     }
 
@@ -56,7 +78,8 @@ $("Document").ready(function() {
     $(".target").prepend("<div class='panel panel-warning nypanel 3star'> <div class='panel panel-body'>3 star ny</div></div>");
     $(".target").prepend("<div class='panel panel-warning nypanel 2star'> <div class='panel panel-body'>2 star ny</div></div>");
     $(".target").prepend("<div class='panel panel-warning nypanel 1star'> <div class='panel panel-body'>1 star ny</div></div>");
-     
+      $('.btn-default').hide();
+    $(".resetSearch").show();
      return; 
       };
 
@@ -75,7 +98,8 @@ $("Document").ready(function() {
     $(".target").prepend("<div class='panel panel-warning papanel 3star'> <div class='panel panel-body'>3 star pa</div></div>");
     $(".target").prepend("<div class='panel panel-warning papanel 2star'> <div class='panel panel-body'>2 star pa</div></div>");
     $(".target").prepend("<div class='panel panel-warning papanel 1star'> <div class='panel panel-body'>1 star pa</div></div>");
-
+ $('.btn-default').hide();
+    $(".resetSearch").show();
       return;
     }
 
