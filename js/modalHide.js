2944b8c56293776321e273e2f1e1c7d1478ccc70
eliezerwohl <eliezerwohl@gmail.com>
2015-11-30 11:13:36 -0500

    made it so the ny and pa have button to remove thier respective panels from search

---
+++ js/modalHide.js
@@ -0,0 +1,48 @@
+$("document").ready(function(){
+  $("#nj5Hide").on("click", function(){
+  $(".nj5").hide();
+  });
+  $("#nj4Hide").on("click", function(){
+  $(".nj4").hide();
+  });
+  $("#nj3Hide").on("click", function(){
+  $(".nj3").hide();
+  });
+  $("#nj2Hide").on("click", function(){
+  $(".nj2").hide();
+  });
+  $("#nj1Hide").on("click", function(){
+  $(".nj1").hide();
+    });
+  $("#ny5Hide").on("click", function(){
+  $(".ny5").hide();
+  });
+  $("#ny4Hide").on("click", function(){
+  $(".ny4").hide();
+  });
+  $("#ny3Hide").on("click", function(){
+  $(".ny3").hide();
+  });
+  $("#ny2Hide").on("click", function(){
+  $(".ny2").hide();
+  });
+  $("#ny1Hide").on("click", function(){
+  $(".ny1").hide();
+  });
+  $("#pa5Hide").on("click", function(){
+  $(".pa5").hide();
+  });
+  $("#pa4Hide").on("click", function(){
+  $(".pa4").hide();
+  });
+  $("#pa3Hide").on("click", function(){
+  $(".pa3").hide();
+  });
+  $("#pa2Hide").on("click", function(){
+  $(".pa2").hide();
+  });
+  $("#pa1Hide").on("click", function(){
+  $(".pa1").hide();
+  });
+
+});
--- js/startup.js
+++ js/startup.js
@@ -1,3 +1,3 @@
-$("document").ready(function(){
-  $(".fakeInvisible").hide();
-});
+// $("document").ready(function(){
+//   $(".fakeInvisible").hide();
+// });
